cc95c8ce80af78ecf62df2017a59bf8fab1dd2ba
Gregory Gerard <foreignbyproxy@gmail.com>
2021-05-12 00:00:14 -0400

    Fix typos on the home page

--- components/Views/Home/Home.tsx
+++ components/Views/Home/Home.tsx
@@ -16,11 +16,10 @@ const Home: React.FC = () => {
 			</p>
 			<h2>About Me</h2>
 			<p>
-				I've been programming since early the early 2010s, but didn't start my web
-				development career until 2016. Besides some VBA macros and a super brief period
-				programming bots in Diablo 3 using C#, my career is focused around web technologies.
-				I work well autonomously and like to learn, experiement, play, build and break
-				things.
+				I've been programming since early 2010s, but didn't start my web development career
+				until 2016. Besides some VBA macros and a super brief period programming bots in
+				Diablo 3 using C#, my career is focused around web technologies. I work well
+				autonomously and like to learn, experiment, play, build and break things.
 			</p>
 
 			<p>
@@ -42,36 +41,36 @@ const Home: React.FC = () => {
 					<strong className="highlight">Performance and monitoring</strong> - At BLD, we
 					implement SEO strategies to help our clients improve their rank in SERPs. Since
 					Google will be incorporating performance metrics into their ranking, we've been
-					busy making sure our client metrics are optimize and are developing a product to
+					busy making sure our client metrics are optimized and developing a product to
 					help monitor and remediate their performance issues.
 				</li>
 				<li>
 					<strong className="highlight">Next.js</strong> - I've been a bit frustrated with
 					the GatsbyJS DX recently so I've been playing and migrating this website over to
-					Next.js. I'm also pushing to build a static site product using Next.js at BLD for
-					clients who need a simple, static solution for their website.
+					Next.js. I'm also pushing to build a static site product using Next.js at BLD
+					for clients who need a simple, static solution for their website.
 				</li>
 			</ul>
 			<h2>Current Situation</h2>
 			<p>
-				I'm the lead developer at Blue Laser Digital. BLD is digital marketing agency in
+				I'm the lead developer at Blue Laser Digital. BLD is a digital marketing agency in
 				Columbus, OH that builds digital strategies and solutions for our clients. I've been
 				at the company for about 5 years.
 			</p>
 			<p>
 				I wear many hats at the company and there is no typical day for me at BLD. I'm
-				responsibile for scoping new projects, communicating and collaborating with our
-				sales and design teams, executing and developing our clients' websites, supporting
-				our internal processes and marketing efforts and developing our product offerings.
+				responsible for scoping new projects, communicating and collaborating with our sales
+				and design teams, executing and developing our clients' websites, supporting our
+				internal processes and marketing efforts and developing our product offerings.
 			</p>
 			<p>
-				Becuase our solutions are tailored to our clients, every project is different. As a
-				results, I've implemented many different solutions across the tech stack. I've done
+				Because our solutions are tailored to our clients, every project is different. As a
+				result, I've implemented many different solutions across the tech stack. I've done
 				everything from building and launching responsive websites and developing
 				progressive web apps using modern Javascript frameworks to helping clients make
 				their website perform better through accessibility and performance audits. I've
-				helped implemented SEO and digital marketing strategies with custom analytics
-				tracking and I've launched e-commerce solutions using different platforms.
+				helped implemente SEO and digital marketing strategies with custom analytics
+				tracking and I've launched e-commerce solutions using a variety of platforms.
 			</p>
 			<p>
 				Check out my{" "}
